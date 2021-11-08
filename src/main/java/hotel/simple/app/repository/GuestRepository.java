ea0afeb70abf6a5afc10df588640e66114657fb0
MohamedSohil85 <mimo_mnb@hotmail.com>
2021-11-08 03:07:29 +0100

    -adding mysql in pox.xml -adding new Package(Repository) for Guest -Implementaion of new Methods(GET)

--- src/main/java/hotel/simple/app/repository/GuestRepository.java
+++ src/main/java/hotel/simple/app/repository/GuestRepository.java
@@ -6,5 +6,6 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
+
 public class GuestRepository implements PanacheRepositoryBase<Guest,Long> {
 }
