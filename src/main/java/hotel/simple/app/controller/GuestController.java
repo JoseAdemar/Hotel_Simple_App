69ae4cd1ca1dc70f69452e30314d7c7aa1963d1a
MohamedSohil85 <mimo_mnb@hotmail.com>
2021-11-08 01:58:48 +0100

    adding mysql in pox.xml adding new Package(Repository) for Guest Implementaion of new Methods(GET)

--- src/main/java/hotel/simple/app/controller/GuestController.java
+++ src/main/java/hotel/simple/app/controller/GuestController.java
@@ -1,18 +1,19 @@
 package hotel.simple.app.controller;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import hotel.simple.app.repository.GuestRepository;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 import hotel.simple.app.entity.Guest;
+
+import java.util.List;
 
 @Path("/api/guest")
 @Produces(MediaType.APPLICATION_JSON)
@@ -20,12 +21,34 @@ import hotel.simple.app.entity.Guest;
 
 public class GuestController {
 
+
+	@Inject
+	GuestRepository guestRepository;
+
 	@POST
 	@Transactional
 	public Response adicionar(@RequestBody @Valid Guest guest) {
 
 		guest.persist();
+
 		return Response.status(Status.CREATED).entity(guest).build();
 	}
-	
+	@Path("/guests")
+	@GET
+	public Response loadAllGuests(){
+		List<Guest>guests=guestRepository.listAll();
+		if (guests.isEmpty()){
+			return Response.noContent().build();
+		}
+		return Response.ok(guests).build();
+	}
+	@Path("/{id}")
+	@GET
+	public Response findGuestById(@PathParam("id")Long id){
+		Guest guest= guestRepository.findById(id);
+		if(!guest.isPersistent()){
+			return Response.noContent().build();
+		}
+		return Response.status(Status.FOUND).entity(guest).build();
+	}
 }
---
+++ src/main/java/hotel/simple/app/repository/GuestRepository.java
@@ -0,0 +1,10 @@
+package hotel.simple.app.repository;
+
+import hotel.simple.app.entity.Guest;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GuestRepository implements PanacheRepositoryBase<Guest,Long> {
+}
